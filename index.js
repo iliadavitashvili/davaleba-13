be8086c87a024cc88b29f11192c4fc5b3c7a7c7f
iliadavitashvili <81050721+iliadavitashvili@users.noreply.github.com>
2024-01-17 14:30:06 +0400

    js primitive values

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+// 1. შევქმნათ ყველა მარტივი ტიპის ცვლადი რაც ვისწავლეთ
+
+let myName = "ilia";
+const myId = 270030206543; // არ არის ჩემი ნამდვილი აიდი
+let isDeveloper = true;
+let adress = undefined;
+let professions = null;
+
+// 2. არითმეტიკული ოპერაციები მიმატება, გამოკლება, გაყოფა, გამრავლება ყველაფერი რაც ვისწავლეთ
+
+let myBankAcc = 0; //default condition
+const mySalary = 1500; //order 1 
+const avrgMonthlyExpenses = 500; //order 2
+
+const amountOfMoneyAfterSalary = myBankAcc + mySalary;
+const amountOfMoneyAfterExpenses = amountOfMoneyAfterSalary - avrgMonthlyExpenses;
+const annualSalary = mySalary * 12;
+const dailyMoneyAmountAfterExpenses = (annualSalary - (avrgMonthlyExpenses * 12)) / 365;
+
+// 3. სტრინგების კონკატენაცია + ით და სტრინგების კონკატენაცია `ბექტიკებით`
+
+let myDescription = "my name is " + myName + " and my ID is " + myId;
+let myDescription2 = `my name is ${myName} and my ID is ${myId} (2)`
+
+
+// 4. მოცემულა მართკუთხედი სიგრძე ცვლადი = 7 და სიგანე = 5 (შექმენით 2 ცვლადი)
+//  გამოთვალეთ მართკუთხედის ფართობი ამ 2 ცვლადის გამოყენებით
+
+const rectangleLength = 7;
+const rectangleWidth = 5;
+const rectangleArea = 7 * 5;
+
+
+
+
+
+
+
+
+
+
